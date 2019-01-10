0b1ecec52f04ed931f686036fc5cf49afd53de9d
Benjamin Constant <35465352+BconstantMMK@users.noreply.github.com>
2019-01-10 19:20:53 +0100

    Nettoyage

--- fonction.h
+++ fonction.h
@@ -5,16 +5,8 @@
 
 void charge (int N, int Np, int Me, int& i1, int& iN);
 
-// int prodMV(int argc, char * argv[], std::vector<std::vector<double> > A, std::vector<double> x);
-
 std::vector<double> prodMVC(std::vector<std::vector<double> > Aloc, std::vector<double> xloc, int nx, int ny);
 
 double dot(std::vector<double> u, std::vector<double> v);
 
-std::vector<double> vectorsplit(std::vector<double> u);
-
-void Diag_init(int nx, int ny, std::vector<std::vector<double> >& A);
-
 std::vector<double> CG (std::vector<std::vector<double> > Aloc, std::vector<double> bloc, std::vector<double> x0loc , double err, int kmax, int nx, int ny);
-
-void printvect( std::vector<double> uloc);
