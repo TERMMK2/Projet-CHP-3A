371f539dfda255b0cc9b3d3662d720f5df4426bc
ArthurLiefhooghe <aliefhooghe@enseirb-matmeca.fr>
2019-01-30 14:44:33 +0100

    ok

--- mainPara.cc
+++ mainPara.cc
@@ -146,7 +146,8 @@ int main(int argc, char *argv[])
 
   if (Me == 0)
   {
-    cout << "Le prog a mis " << t * 0.000001 << " secondes a s'effectuer" << endl << "Writing Data...";
+    cout  << "Le prog a mis " << t * 0.000001 << " secondes a s'effectuer" << endl 
+          << "Writing Data..." << endl;
     Lap.write_record_data();
   }
 
